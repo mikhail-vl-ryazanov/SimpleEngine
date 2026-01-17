93a90181152363235789c6fc8b7d1b3792239456
mikhail-vl-ryazanov <mikhail.vl.ryazanov@yandex.ru>
2026-01-17 13:33:12 +0300

    Initial commit

---
+++ SimpleEngineEditor/src/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <memory>
+
+#include <SimpleEngineCore/Application.hpp>
+
+class MyApp : public SimpleEngine::Application
+{
+	virtual void on_update() override
+	{
+		std::cout << "Update frame : " << frame++ << std::endl;
+	}
+
+	int frame = 0;
+};
+
+int main()
+{
+
+	auto myApp = std::make_unique<MyApp>();
+
+	int returnCode = myApp->start(1024, 768, "My first App");
+
+	std::cin.get();
+
+	return returnCode;
+}
