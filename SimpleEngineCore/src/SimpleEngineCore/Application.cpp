08a135da7bfbaf851c3fbf82acebe8c704956ae3
mikhail-vl-ryazanov <mikhail.vl.ryazanov@yandex.ru>
2026-01-17 14:43:38 +0300

    Added Application, spdlog, glfw, glad

--- SimpleEngineCore/src/SimpleEngineCore/Application.cpp
+++ SimpleEngineCore/src/SimpleEngineCore/Application.cpp
@@ -1,8 +1,9 @@
-#pragma once
-
 #include "SimpleEngineCore/Application.hpp"
+#include "SimpleEngineCore/Log.hpp"
 
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
+
 
 #include <iostream>
 
@@ -10,6 +11,14 @@ namespace SimpleEngine {
 
 	Application::Application()
 	{
+        LOG_INFO("Welcome to spdlog!");
+        LOG_ERROR("Some error message with arg: {}", 1);
+
+        LOG_WARN("Easy padding in numbers like {:08d}", 12);
+        LOG_CRITICAL("Support for int: {0:d};  hex: {0:x};  oct: {0:o}; bin: {0:b}", 42);
+        LOG_INFO("Support for floats {:03.2f}", 1.23456);
+        LOG_INFO("Positional args are {1} {0}..", "too", "supported");
+        LOG_INFO("{:<30}", "left aligned");
 
 	}
 
@@ -20,9 +29,6 @@ namespace SimpleEngine {
 
 	int Application::start(unsigned int window_width, unsigned int window_height, const char* title)
 	{
-        std::cout << "Hello from Simple Engine Core" << std::endl;
-
-
         GLFWwindow* window;
 
         /* Initialize the library */
@@ -40,11 +46,20 @@ namespace SimpleEngine {
         /* Make the window's context current */
         glfwMakeContextCurrent(window);
 
+
+        if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+        {
+            LOG_CRITICAL("Failed to initialize GLAD");
+            return -1;
+        }
+
+        glClearColor(1, 0, 0, 0);
+
         /* Loop until the user closes the window */
         while (!glfwWindowShouldClose(window))
         {
             /* Render here */
-            //glClear(GL_COLOR_BUFFER_BIT);
+            glClear(GL_COLOR_BUFFER_BIT);
 
             /* Swap front and back buffers */
             glfwSwapBuffers(window);
