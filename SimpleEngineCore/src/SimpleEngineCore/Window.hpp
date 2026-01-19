baf9960c030f6ef00ba26a7a08e7dfcd8624255c
mikhail-vl-ryazanov <mikhail.vl.ryazanov@yandex.ru>
2026-01-19 11:51:48 +0300

    Added ImGUI

--- SimpleEngineCore/src/SimpleEngineCore/Window.hpp
+++ SimpleEngineCore/src/SimpleEngineCore/Window.hpp
@@ -23,7 +23,7 @@ namespace SimpleEngine {
 		Window& operator=(Window&&) = delete;
 
 		void on_update();
-		unsigned int get_widht() const { return m_data.width;  }
+		unsigned int get_width() const { return m_data.width;  }
 		unsigned int get_height() const { return m_data.height; }
 
 		void set_event_callback(const EventCallbackFn& callback)
