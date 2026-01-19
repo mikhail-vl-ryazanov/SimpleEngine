c41e17db476e6355297378e530ba9cdeb4cdf76b
mikhail-vl-ryazanov <mikhail.vl.ryazanov@yandex.ru>
2026-01-19 21:27:00 +0300

    Added BufferLayout

--- SimpleEngineCore/src/SimpleEngineCore/Rendering/OpenGL/VertexArray.cpp
+++ SimpleEngineCore/src/SimpleEngineCore/Rendering/OpenGL/VertexArray.cpp
@@ -48,10 +48,18 @@ namespace SimpleEngine {
 		bind();
 		vertex_buffer.bind();
 
-		//TODO - use buffer layout
-		glEnableVertexAttribArray(m_elements_count);
-		glVertexAttribPointer(m_elements_count, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
-
-		++m_elements_count;
+		for (const BufferElement& current_element : vertex_buffer.get_layout().get_elements())
+		{
+			glEnableVertexAttribArray(m_elements_count);
+			glVertexAttribPointer(
+				m_elements_count, 
+				static_cast<GLint>(current_element.components_count), 
+				current_element.component_type, 
+				GL_FALSE, 
+				static_cast<GLsizei>(vertex_buffer.get_layout().get_stride()),
+				reinterpret_cast<const void*>(current_element.offset)
+			);
+			++m_elements_count;
+		}
 	}
 }
