dade9fc063c371c2966953541ef69f22547c9db5
mikhail-vl-ryazanov <mikhail.vl.ryazanov@yandex.ru>
2026-01-20 10:53:39 +0300

    Added IndexBuffer

--- SimpleEngineCore/src/SimpleEngineCore/Rendering/OpenGL/VertexArray.cpp
+++ SimpleEngineCore/src/SimpleEngineCore/Rendering/OpenGL/VertexArray.cpp
@@ -43,7 +43,7 @@ namespace SimpleEngine {
 		glBindVertexArray(0);
 	}
 
-	void VertexArray::add_buffer(const VertexBuffer& vertex_buffer)
+	void VertexArray::add_vertex_buffer(const VertexBuffer& vertex_buffer)
 	{
 		bind();
 		vertex_buffer.bind();
@@ -61,5 +61,12 @@ namespace SimpleEngine {
 			);
 			++m_elements_count;
 		}
+	}
+
+	void VertexArray::set_index_buffer(const IndexBuffer& index_buffer)
+	{
+		bind();
+		index_buffer.bind();
+		m_indices_count = index_buffer.get_count();
 	}
 }
